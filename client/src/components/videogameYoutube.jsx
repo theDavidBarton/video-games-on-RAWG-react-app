a3babde2927991e112755b530ebf62b90664f6c5
David Barton <barton.david.sv@gmail.com>
2020-01-28 17:35:34 +0100

    fix arrow icons for real

--- client/src/components/videogameYoutube.jsx
+++ client/src/components/videogameYoutube.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import RightArrow from './mediaRightArrow'
 
 class Youtube extends Component {
   render() {
     const youtubeElement = this.props.data.thumbnails.medium.url
     return (
       <div className='col-md-3 my-3'>
-        <span className='position-absolute bg-dark text-light youtube-play-style'>►</span>
+        <span className='position-absolute bg-dark text-light youtube-play-style'>
+          <RightArrow />
+        </span>
         {youtubeElement ? (
           <img className={`img-style ${this.props.class}`} src={youtubeElement} alt='screenshot of the video' />
         ) : null}
