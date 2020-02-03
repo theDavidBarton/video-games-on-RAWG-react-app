3d5c3502e60c5e6de7293fdd34185819a60e128f
David Barton <barton.david.sv@gmail.com>
2020-02-03 17:46:10 +0100

    sanitize dangerouslySetInnerHTML-s

--- client/src/components/videogameReview.jsx
+++ client/src/components/videogameReview.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, Component } from 'react'
+import dompurify from 'dompurify'
 
 class Review extends Component {
   render() {
@@ -6,7 +7,7 @@ class Review extends Component {
     return (
       <Fragment>
         <div>{'★'.repeat(reviewElement.rating) + '☆'.repeat(5 - reviewElement.rating)}</div>
-        <p dangerouslySetInnerHTML={{ __html: reviewElement.text }}></p>
+        <p dangerouslySetInnerHTML={{ __html: dompurify.sanitize(reviewElement.text) }}></p>
         <strong>by {reviewElement.user ? reviewElement.user.username : reviewElement.external_author} </strong>
         <small>{reviewElement.created ? reviewElement.created.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/) : 'n/a'}</small>
         <hr />
