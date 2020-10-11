54d86e2db9f8b63c20170e0eaaa6349ecf1f53fe
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2020-10-11 10:48:14 +0200

    revert "code split lazy load components (#74)" (#76)
    this reverts commit 00f090bc49dde21ada2fab0da60fc4e9cf33a8cb.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,33 +1,25 @@
-import React, { Suspense, lazy } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-const Videogame = lazy(() => import('./videogame'))
-const Page404 = lazy(() => import('./404'))
-const CookieBar = lazy(() => import('./cookieBar'))
-const Header = lazy(() => import('./header'))
-const Footer = lazy(() => import('./footer'))
-const Homepage = lazy(() => import('./homepage'))
+import Videogame from './videogame'
+import Page404 from './404'
+import CookieBar from './cookieBar'
+import Header from './header'
+import Footer from './footer'
+import Homepage from './homepage'
 
 export default function App() {
   return (
     <div className='App'>
-      <Suspense fallback={<div className='bg-dark'></div>}>
-        <CookieBar />
-      </Suspense>
-      <Suspense fallback={<div className='bg-dark'></div>}>
-        <Header />
-      </Suspense>
+      <CookieBar />
+      <Header />
       <BrowserRouter>
-        <Suspense fallback={<div className='bg-dark'>Loading...</div>}>
-          <Switch>
-            <Route exact path='/' component={Homepage} />
-            <Route path='/videogame/:id' component={Videogame} />
-            <Route component={Page404} />
-          </Switch>
-        </Suspense>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/videogame/:id' component={Videogame} />
+          <Route component={Page404} />
+        </Switch>
       </BrowserRouter>
-      <Suspense fallback={<div className='bg-dark'></div>}>
-        <Footer />
-      </Suspense>
+      <Footer />
     </div>
   )
 }
